10cdaa378576c9db9253eec5d476f2e484345c02
sea-snake <sea-snake@outlook.com>
2022-05-01 16:16:54 +0200

    Split transfer methods into transfer and transfer_from methods to clearly separate user and approved transfers following the erc721 spec. Replace camel case methods with snake case methods.

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,12 @@ extern crate core;
 
 use std::cell::RefCell;
 use std::collections::HashMap;
-use std::convert::{TryInto};
+use std::convert::TryInto;
 
 use candid::Nat;
 use ic_cdk::{caller, id, storage, trap};
+use ic_cdk::api::canister_balance;
 use ic_cdk::api::call::{msg_cycles_accept, msg_cycles_available};
-use ic_cdk::api::{canister_balance};
 use ic_cdk::export::candid::CandidType;
 use ic_cdk::export::Principal;
 use ic_cdk_macros::{init, post_upgrade, pre_upgrade, query, update};
@@ -52,38 +52,45 @@ thread_local! {
     static STATE: RefCell<State> = RefCell::new(State::default());
 }
 
-#[query(name = "balanceOf")]
+#[query]
 fn balance_of(user: User) -> Nat {
     STATE.with(|s| Nat::from(s.borrow().tokens.iter().filter(|token| token.owner == user).count()))
 }
 
-#[query(name = "ownerOf")]
-fn owner_of(token_id: Principal, user: User) -> bool {
-    STATE.with(|s| s.borrow().tokens.iter().find(|token| token.id == token_id && token.owner == user).is_some())
+#[query]
+fn owner_of(token_id: Principal) -> Result<User, String> {
+    STATE.with(|s| {
+        let state = s.borrow();
+        let token = state.tokens
+            .iter()
+            .find(|token| token.id == token_id)
+            .ok_or("Token not found")?;
+        Ok(token.owner)
+    })
 }
 
-#[query(name = "name")]
+#[query]
 fn name() -> String {
     STATE.with(|s| s.borrow().name.clone())
 }
 
-#[query(name = "symbol")]
+#[query]
 fn symbol() -> String {
     STATE.with(|s| s.borrow().symbol.clone())
 }
 
-#[query(name = "totalSupply")]
+#[query]
 fn total_supply() -> Nat {
     STATE.with(|s| Nat::from(s.borrow().tokens.len()))
 }
 
-#[update(name = "approve")]
+#[update]
 async fn approve(from_subaccount: Option<Subaccount>, approved: Principal, token_id: Principal, is_approved: bool) -> Result<(), String> {
     if approved == caller() {
         return Err("Caller cannot approve itself".to_string());
     }
     STATE.with(|s| {
-        let state = s.borrow();
+        let mut state = s.borrow_mut();
         let token = state.tokens
             .iter()
             .find(|token| token.id == token_id)
@@ -107,18 +114,18 @@ async fn approve(from_subaccount: Option<Subaccount>, approved: Principal, token
                         }
                     }
                     None => {
-                        s.borrow_mut().approved.insert(token_id, vec![approved]);
+                        state.approved.insert(token_id, vec![approved]);
                         return Ok(());
                     }
                 }
-                s.borrow_mut().approved.get_mut(&token_id).unwrap().push(approved);
+                state.approved.get_mut(&token_id).unwrap().push(approved);
             }
             false => {
                 let index = state.approved
                     .get(&token_id)
                     .and_then(|a| a.iter().position(|a| a == &approved))
                     .ok_or("Approved not found")?;
-                s.borrow_mut().approved.get_mut(&token_id).unwrap().remove(index);
+                state.approved.get_mut(&token_id).unwrap().remove(index);
             }
         }
         Ok(())
@@ -133,36 +140,36 @@ async fn approve(from_subaccount: Option<Subaccount>, approved: Principal, token
     Ok(())
 }
 
-#[query(name = "isApproved")]
+#[query]
 fn is_approved(token_id: Principal) -> bool {
     let spender = caller();
     STATE.with(|s| s.borrow().approved.get(&token_id).map_or(false, |approved| approved.contains(&spender)))
 }
 
-#[query(name = "getApproved")]
+#[query]
 fn get_approved(token_id: Principal) -> Vec<Principal> {
     STATE.with(|s| s.borrow().approved.get(&token_id).unwrap_or(&Vec::<Principal>::new()).clone())
 }
 
-#[update(name = "transfer")]
+#[update]
 async fn transfer(from_subaccount: Option<Subaccount>, to: User, token_id: Principal) -> Result<(), String> {
     if to == User::Null {
         return Err("Cannot transfer to null".to_string());
     }
+    let from = match from_subaccount {
+        Some(subaccount) => User::Account(AccountIdentifier::new(&caller(), &subaccount)),
+        None => User::Principal(caller())
+    };
     STATE.with(|s| {
         let mut state = s.borrow_mut();
-        let is_approved = state.approved.get(&token_id).and_then(|approved| Some(approved.contains(&caller()))).unwrap_or(false);
         let is_custodian = state.custodians.contains(&caller());
         let token = state.tokens
             .iter_mut()
             .find(|token| token.id == token_id)
             .ok_or("Token not found")?;
-        let is_owner = match from_subaccount {
-            Some(subaccount) => User::Account(AccountIdentifier::new(&caller(), &subaccount)),
-            None => User::Principal(caller())
-        } == token.owner;
-        if !is_owner && !(token.owner == User::Null && is_custodian) && !is_approved {
-            return Err("Caller is not owner, custodian or approved".to_string());
+        let is_owner = from == token.owner;
+        if !is_owner && !(token.owner == User::Null && is_custodian) {
+            return Err("Caller is not owner or custodian".to_string());
         }
         token.owner = to.clone();
         Ok(())
@@ -174,13 +181,48 @@ async fn transfer(from_subaccount: Option<Subaccount>, to: User, token_id: Princ
         let _: Result<(), _> = ic_cdk::api::call::call(
             events,
             "transfer_event",
-            (from_subaccount, to, token_id, ),
+            (from, to, token_id, ),
         ).await;
     }
     Ok(())
 }
 
-#[update(name = "mint")]
+#[update]
+async fn transfer_from(from: User, to: User, token_id: Principal) -> Result<(), String> {
+    if to == User::Null {
+        return Err("Cannot transfer to null".to_string());
+    }
+    STATE.with(|s| {
+        let mut state = s.borrow_mut();
+        let is_approved = state.approved.get(&token_id).and_then(|approved| Some(approved.contains(&caller()))).unwrap_or(false);
+        let token = state.tokens
+            .iter_mut()
+            .find(|token| token.id == token_id)
+            .ok_or("Token not found")?;
+        let is_owner = from == token.owner;
+        if !is_owner {
+            return Err("Token is not owned by user passed as from argument".to_string());
+        }
+        if !is_approved {
+            return Err("Caller is not approved".to_string());
+        }
+        token.owner = to.clone();
+        Ok(())
+    })?;
+    STATE.with(|s| {
+        s.borrow_mut().approved.remove(&token_id);
+    });
+    if let Some(events) = STATE.with(|s| s.borrow().events) {
+        let _: Result<(), _> = ic_cdk::api::call::call(
+            events,
+            "transfer_event",
+            (from, to, token_id, ),
+        ).await;
+    }
+    Ok(())
+}
+
+#[update]
 async fn mint() -> Result<Token, String> {
     let token = STATE.with(|s| {
         let mut state = s.borrow_mut();
@@ -199,7 +241,7 @@ async fn mint() -> Result<Token, String> {
         }
         let token = Token {
             id: Principal::from_slice([
-                b"\nsld",
+                b"\x0Asld",
                 id().as_slice(),
                 mint.to_be_bytes().as_slice(),
                 &[1] // Opaque identifier
@@ -219,7 +261,7 @@ async fn mint() -> Result<Token, String> {
     Ok(token)
 }
 
-#[update(name = "burn")]
+#[update]
 async fn burn(token_id: Principal) -> Result<(), String> {
     STATE.with(|s| {
         let index = s.borrow().tokens
@@ -245,41 +287,55 @@ async fn burn(token_id: Principal) -> Result<(), String> {
     Ok(())
 }
 
-#[query(name = "tokens")]
+#[query]
 fn tokens() -> Vec<Token> {
     STATE.with(|s| s.borrow().tokens.clone())
 }
 
-#[query(name = "tokensOf")]
+#[query]
 fn tokens_of(owner: User) -> Vec<Token> {
     STATE.with(|s| s.borrow().tokens.iter().filter(|token| token.owner == owner).cloned().collect::<Vec<Token>>())
 }
 
-#[query(name = "extensions")]
+#[query]
 fn extensions() -> HashMap<Principal, Vec<String>> {
     STATE.with(|s| s.borrow().extensions.clone())
 }
 
-#[update(name = "setExtensions")]
-fn set_extensions(extensions: HashMap<Principal, Vec<String>>) -> Result<(), String> {
+#[update]
+fn set_extension(principal: Principal, types: Vec<String>) -> Result<(), String> {
     STATE.with(|s| {
-        if !s.borrow().custodians.contains(&caller()) {
+        let mut state = s.borrow_mut();
+        if !state.custodians.contains(&caller()) {
             return Err("Caller is not a custodian".to_string());
         }
-        s.borrow_mut().extensions = extensions;
+        match types.len() {
+            0 => {
+                state.extensions.remove(&principal);
+            }
+            l if l > 256 => {
+                return Err("Max extension types (256) has been reached".to_string());
+            }
+            _ => {
+                if state.extensions.keys().len() == 256 {
+                    return Err("Max extensions (256) has been reached".to_string());
+                }
+                state.extensions.insert(principal, types);
+            }
+        }
         Ok(())
     })
 }
 
-#[query(name = "custodians")]
+#[query]
 fn custodians() -> Vec<Principal> {
     STATE.with(|s| s.borrow().custodians.clone())
 }
 
-#[update(name = "setCustodian")]
+#[update]
 fn set_custodian(custodian: Principal, is_custodian: bool) -> Result<(), String> {
     STATE.with(|s| {
-        let state = s.borrow();
+        let mut state = s.borrow_mut();
         if !state.custodians.contains(&caller()) {
             return Err("Caller is not a custodian".to_string());
         }
@@ -291,31 +347,31 @@ fn set_custodian(custodian: Principal, is_custodian: bool) -> Result<(), String>
                 if state.custodians.len() == 256 {
                     return Err("Max custodians (256) has been reached".to_string());
                 }
-                s.borrow_mut().custodians.push(custodian);
+                state.custodians.push(custodian);
             }
             false => {
                 let index = state.custodians
                     .iter()
                     .position(|c| c == &custodian)
                     .ok_or("Custodian not found")?;
-                s.borrow_mut().custodians.remove(index);
+                state.custodians.remove(index);
             }
         }
         Ok(())
     })
 }
 
-#[query(name = "events")]
+#[query]
 fn events() -> Option<Principal> {
     STATE.with(|s| s.borrow().events)
 }
 
-#[update(name = "setEvents")]
+#[update]
 fn set_events(events: Option<Principal>) {
     STATE.with(|s| s.borrow_mut().events = events)
 }
 
-#[query(name = "cycles")]
+#[query]
 fn cycles() -> Nat {
     Nat::from(canister_balance())
 }
